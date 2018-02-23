cc1666f6adb9261e19e2636b98a5e257a3038c06
NoharaRin <36765328+NoharaRin@users.noreply.github.com>
2018-02-23 20:30:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+﻿const fs = require("fs");
+const login = require("facebook-chat-api");
+
+login({email: "thopk007113@gmail.com", password: "NoharaRin"}, (err, api) => {
+    if(err) return console.error(err);
+	api.listen(function callback(err, message) {		
+	 if (message.body){
+			api.sendMessage("- Tui đang bận! \n- Tui sẽ trả lời tin nhắn của bạn sau khi tui seen <3", message.threadID);
+	        return;
+		}
+	});
+});
